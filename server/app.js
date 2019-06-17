5656d4c0e2a34611c0ca35877596c79cab50d98c
Liel van der Hoeven <lielvan1234@gmail.com>
2019-06-17 20:09:25 +0300

    setup initial frontend & backend

--- server/app.js
+++ server/app.js
@@ -11,6 +11,14 @@ app.use(cors());
 const chapters = require('./routes/api/chapters');
 app.use('/api/chapters', chapters);
 
-const port = process.env.PORT || 5000;
+// Handle Production
+if(process.env.NODE_ENV === 'production') {
+  // Static folder
+  app.use(express.static(__dirname + '/public/'));
 
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));

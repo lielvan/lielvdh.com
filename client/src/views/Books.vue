dca9668b851d8fdf93503b4d98399129674ef095
Liel van der Hoeven <lielvan1234@gmail.com>
2019-07-10 22:52:08 +0300

    responsive style books

--- client/src/views/Books.vue
+++ client/src/views/Books.vue
@@ -1,14 +1,18 @@
 <template>
-  <section class="hero">
-    <h2 class="title is-2 hidden-fade-in-left section-title" v-fadein="'showElement'">How I Waste <br> My Spare Time</h2>
-    <div class="books-wrapper hero-body">
-      <carousel :autoplay="true" :autoplayHoverPause="true" :perPageCustom="[[768, 4], [1024, 5]]">
-        <slide v-for="(book, index) in books" :item="book" :index="index" :key="book._id">
-          <img :src="book.image" alt="Book Image Unavailable">
-        </slide>
-      </carousel>
-    </div>
-  </section>
+  <div class="books-wrapper hero-body">
+    <p class="subtitle is-size-7-mobile is-size-6-tablet">Reading thrillers and fantasy has always been a joy <br> But also self-improvement books (gotta keep that brain active)<br> Here are few of my favorites!</p>
+    <carousel :autoplay="true" :perPageCustom="[[768, 3], [1215, 4], [1407, 5]] ">
+      <slide v-for="(book, index) in books" :item="book" :index="index" :key="book._id">
+        <div class="book-img-wrapper">
+          <img class="book-img" :src="book.image" alt="Book Image Unavailable">
+          <div class="book-info">
+            <!-- <span id="book-rating"><a :href="this.book_link" target="_blank"><img class="goodreads-logo" src="/images/goodreads_logo.png" alt="GoodReads:"></a> {{ rating }} / 5</span> -->
+            <p id="book-description">{{ book.description }}</p>
+          </div>
+        </div>
+      </slide>
+    </carousel>
+  </div>
 </template>
 
 <script>
@@ -33,8 +37,55 @@ export default {
 </script>
 
 <style>
-/* .VueCarousel-slide {
-  background-color: blue;
-  border: 1px solid black;
+.book-img {
+  box-shadow: 3px 2px 5px 1px rgba(0,0,0,0.75);
+  border-radius: 6px;
+}
+.VueCarousel-slide {
+  position: relative;
+}
+.book-img-wrapper {
+  transition: all 0.5s ease;
+}
+/* .book-img-wrapper:hover {
+  transform: scale(1.08);
 } */
+.book-img-wrapper .book-info {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  position: absolute;
+  padding: 1rem;
+  left: 19.5%;
+  top: 2.5%;
+  height: 93%;
+  width: 232px;
+  border-radius: 6px;
+  color: #000;
+  background-color: rgba(255, 255, 255, 0.9);
+  opacity: 0;
+  transition: opacity 0.5s ease-in-out;
+}
+.book-img-wrapper:hover .book-info {
+    opacity: 1;
+}
+.book-info #book-description {
+  font-size: 0.85rem;
+  line-height: 1.5rem;
+}
+@media only screen and (min-width: 320px) {
+  .book-img {
+    width: 125px;
+  }
+}
+@media only screen and (min-width: 425px) {
+  .book-img {
+    width: 180px;
+  }
+}
+@media only screen and (min-width: 1024px) {
+  .book-img {
+    width: initial;
+  }
+}
 </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -35,8 +35,11 @@
 
     <Head />
     <Chapters />
-    <Books />
     <!-- Hobbies: Lifting/Swimming, Futball, Reading, Gradening, Investing -->
+    <section class="hero">
+      <h1 class="title is-2 hidden-fade-in-left section-title" v-fadein="'showElement'">How I Waste <br> My Spare Time</h1>
+      <Books />
+    </section>
     <Motos />
     <Footer />
     

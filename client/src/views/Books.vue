5c5bcf98c040cfa6c45e2a3b2e37ee140fe7c37d
Liel van der Hoeven <lielvan1234@gmail.com>
2019-07-15 19:00:37 +0300

    Adjust some book-info responsive styles

--- client/src/views/Books.vue
+++ client/src/views/Books.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="books-wrapper hero-body">
     <p class="subtitle is-size-7-mobile is-size-6-tablet">Reading thrillers and fantasy has always been a joy <br> But also self-improvement books (gotta keep that brain active)<br> Here are few of my favorites!</p>
-    <carousel :autoplay="true" :perPageCustom="[[768, 3], [1215, 4], [1407, 5]] ">
+    <carousel :autoplay="false" :perPageCustom="[[768, 3], [1215, 4], [1407, 5]] ">
       <slide v-for="(book, index) in books" :item="book" :index="index" :key="book._id">
         <div class="book-img-wrapper">
           <img class="book-img" :src="book.image" alt="Book Image Unavailable">
           <div class="book-info">
             <!-- <span id="book-rating"><a :href="this.book_link" target="_blank"><img class="goodreads-logo" src="/images/goodreads_logo.png" alt="GoodReads:"></a> {{ rating }} / 5</span> -->
-            <p id="book-description">{{ book.description }}</p>
+            <p id="book-description" class="is-size-7-tablet">{{ book.description }}</p>
           </div>
         </div>
       </slide>
@@ -47,18 +47,15 @@ export default {
 .book-img-wrapper {
   transition: all 0.5s ease;
 }
-/* .book-img-wrapper:hover {
-  transform: scale(1.08);
-} */
 .book-img-wrapper .book-info {
   display: flex;
   justify-content: center;
   flex-direction: column;
   position: absolute;
-  padding: 1rem;
-  left: 19.5%;
+  padding: 15px;
+  left: 48px;
   top: 2.5%;
-  height: 93%;
+  height: 355px;
   width: 232px;
   border-radius: 6px;
   color: #000;
@@ -69,23 +66,46 @@ export default {
 .book-img-wrapper:hover .book-info {
     opacity: 1;
 }
-.book-info #book-description {
+#book-description {
   font-size: 0.85rem;
   line-height: 1.5rem;
 }
-@media only screen and (min-width: 320px) {
+@media only screen and (min-width: 320px) and (max-width: 424px) {
   .book-img {
-    width: 125px;
+    width: 150px;
   }
+  .book-img-wrapper .book-info {
+    left: 22px;
+    height: 212px;
+    width: 139px;
+  }
+  #book-description {
+    font-size: 0.6rem;
+    line-height: initial;
+  }
+
 }
-@media only screen and (min-width: 425px) {
+@media only screen and (min-width: 425px) and (max-width: 1023px) {
   .book-img {
     width: 180px;
+  }
+  .book-img-wrapper .book-info {
+    left: 34px;
+    height: 257px;
+    width: 171px;
+  }
+  #book-description {
+    line-height: initial;
   }
 }
 @media only screen and (min-width: 1024px) {
   .book-img {
     width: initial;
+  }
+}
+@media only screen and (min-width: 1408px) {
+  .book-img-wrapper .book-info {
+    left: 23px;
   }
 }
 </style>
